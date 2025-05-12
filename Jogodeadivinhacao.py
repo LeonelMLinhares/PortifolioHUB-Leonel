148332b3cadaefef2bc6de9ca3ad814186a1acc6
LeonelMLinhares <leonel.m.linhares@gmail.com>
2025-05-11 23:11:23 -0300

    Add files via upload

---
+++ Jogodeadivinhacao.py
@@ -0,0 +1,19 @@
+import random
+
+print("=== JOGO DE ADIVINHAÇÃO ===")
+print("Tente adivinhar o número entre 1 e 100!")
+
+numero_secreto = random.randint(1, 100)
+tentativas = 0
+
+while True:
+    palpite = int(input("Digite seu palpite: "))
+    tentativas += 1
+    
+    if palpite == numero_secreto:
+        print(f"Parabéns! Você acertou em {tentativas} tentativas!")
+        break
+    elif palpite < numero_secreto:
+        print("Tente um número MAIOR!")
+    else:
+        print("Tente um número MENOR!")
